7d45b29d0e877bb2852d317c481f8f0905f7940f
Daniel <63756498+Abuyedaniel@users.noreply.github.com>
2022-06-22 04:34:01 +0100

    final commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let Name = "Abuye Daniel Oghenechovwe \n"
-let Height = "5'6 \n"
+let Height = "5 feet 6 inches \n"
 let Country = "Nigeria \n"
 
 console.log("Name:" ,Name, "Height:" ,Height, "Country:" ,Country)
